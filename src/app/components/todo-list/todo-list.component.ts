b618cc6b44ba2bc1c6831b02b22696d813283596
Nicholas Balarezo <143428049+ngbalarezo@users.noreply.github.com>
2025-04-02 13:31:13 -0400

    Added task list functionality to todo-list component.

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TodoItem } from '../../models/todo-item';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'todo-list',
-  imports: [FormsModule],
+  imports: [FormsModule, NgFor],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
@@ -25,7 +26,7 @@ export class TodoListComponent {
 
       this.items.push(newItem);
     }
-    
+
     this.newTask = '';
   }
 
